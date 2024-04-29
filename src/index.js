dee4ae8689c2a225e3638644a0714fbb153f4dea
hackdanismo <office@justtoconfirm.co.uk>
2024-04-29 17:45:18 +0100

    chore: allow the Input component to be exported

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from "./components/button/index.jsx"
 export { default as Image } from "./components/image/index.jsx"
+export { default as Input } from "./components/input/index.jsx"
 export { default as Link } from "./components/link/index.jsx"
